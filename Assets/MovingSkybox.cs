e37dc7ee25ab0dc7437a8dddf7e66bc3a0fafc00
HyunminCho <ggstargame45@gmail.com>
2024-04-26 13:37:23 +0900

    first commit

---
+++ Assets/MovingSkybox.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSkybox : MonoBehaviour
+{
+    public float speed;
+    
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+        
+    }
+}
